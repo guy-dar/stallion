2b0943c7051cf4ce942abe9978fd6c721f94870c
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2020-04-22 00:24:17 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1861,7 +1861,7 @@ gulp.task("externaltest", function (done) {
 gulp.task(
   "npm-test",
   gulp.series(
-    // gulp.parallel("lint", "externaltest", "unittestcli"),
-    "lint-chromium"
+    gulp.parallel("lint", "externaltest", "unittestcli"),
+    // "lint-chromium"
   )
 );
